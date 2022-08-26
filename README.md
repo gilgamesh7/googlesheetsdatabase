dc6e48be2b832eb053a566b4176241a8c11e4ca1
gilgamesh7 <felidaesrule@yahoo.com>
2022-08-26 17:16:02 +1200

    create & update sheet

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # googlesheetsdatabase
 
 #[Turning Google Sheets into a Database with Python](https://towardsdatascience.com/turn-google-sheets-into-your-own-database-with-python-4aa0b4360ce7)
+- Creates a new spreadsheet and updates it with data from a local file
 
 # Installation
 - pip install gspread
--- app.py
+++ app.py
@@ -10,13 +10,27 @@ logger = logging.getLogger("GOOGLESHEETSDB")
 
 try:
     # Connect to Google Sheets
-    logger.info("Connecting to Google Sheets...")
+    logger.info("Connecting to Google Sheets")
 
     scope = ['https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive"]
     credentials = ServiceAccountCredentials.from_json_keyfile_name("certificates/google_sheets_credentials.json", scope)
     client = gspread.authorize(credentials)
 
-    logger.info("Connected to Google Sheets")
+    logger.info("Create sheet")
+    sheet = client.create("NewDatabase")
+    logger.info("Share with me")
+    sheet.share('felidaesrule@gmail.com', perm_type='user', role='writer')
+
+    logger.info("Open sheet")
+    sheet = client.open("NewDatabase").sheet1
+
+    logger.info("Read local CSV into Pandas")
+    df = pd.read_csv('sheets/football_news.csv')
+
+    logger.info("Updating new sheet with data from local file")
+    sheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+    logger.info("Completed")
 
 except Exception as error:
     logger.error(f"{error}")
